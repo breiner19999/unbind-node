b5fb6bd1cf7699c0cf923f0cdd9853ae443ad31b
breiner19999 <breinerbca2015@gmail.com>
2022-09-23 09:35:21 -0500

    añadiendo puerto para heroku

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 'use strict'
 
 var app = require('./app');
-/* var port = process.env.PORT || 8080; */
-var port = 8080;
+var port = process.env.PORT || 8080;
+/* var port = 8080; */
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 app.listen(port, function() {
   console.log('Servidor escuchando:' + port);
